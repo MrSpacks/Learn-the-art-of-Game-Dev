67202d13de9f94bee4f04972efee231899439aff
Mr. Spacks <94480894+MrSpacks@users.noreply.github.com>
2024-04-08 20:07:25 +0200

    add Java Script code

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const nav = document.querySelector("nav");
+const mobilNav = document.querySelector("nav.mobile_nav");
 
 window.addEventListener("scroll", () => {
-        if (window.pageXOffset > 60) {
+        if (window.scrollY > 60) {
             nav.classList.add("scrolled");
+            mobilNav.classList.add("scrolled");
+            console.log("add class");
         } else {
             nav.classList.remove("scrolled");
+            mobilNav.classList.remove("scrolled");
+            console.log("remove class");
+
         }
 });
